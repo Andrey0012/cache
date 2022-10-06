1e4b99f7f3fa2dd6ae6ce234386a1413990ca87e
Andrey0012 <andrey.0012@mail.ru>
2022-10-06 20:46:30 +0300

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/springboot/stringboot_rest/StringbootRestApplication.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.springboot.stringboot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringbootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringbootRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaurtregulov/spring/springboot/stringboot_rest/StringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.springboot.stringboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
